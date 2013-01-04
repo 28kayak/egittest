68a4f8a3a34e12f4bcc52485ddfb9611260d36ba
kaya <kaya_ota@msn.com>
2013-01-04 01:24:36 -0800

    add one line

--- testJuno/src/test.java
+++ testJuno/src/test.java
@@ -4,7 +4,9 @@ public class test
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		System.out.println("hello world");
+		System.out.println("this is kayak");
+		
 		
 
 	}
