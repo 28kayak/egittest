2a72dd7fdb4ff435866ef985a911596600fe7f0f
kaya <kaya_ota@msn.com>
2013-01-05 02:29:48 -0800

    change name to be the account name

--- testJuno/src/test.java
+++ testJuno/src/test.java
@@ -5,7 +5,7 @@ public class test
 	public static void main(String[] args)
 	{
 		System.out.println("hello world");
-		System.out.println("this is kayak");
+		System.out.println("this is 28kayak");
 		
 		
 
